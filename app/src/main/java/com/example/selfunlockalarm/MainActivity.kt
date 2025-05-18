07ed38a4794c3fe8694e7ec94889143bece4bbe1
kuwa <h.kuwabara1103@gmail.com>
2025-05-18 18:03:25 +0900

    ActivityにHilt設定

--- app/src/main/java/com/example/selfunlockalarm/MainActivity.kt
+++ app/src/main/java/com/example/selfunlockalarm/MainActivity.kt
@@ -7,13 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.selfunlockalarm.ui.AlarmScreen
 import com.example.selfunlockalarm.ui.theme.SelfUnlockAlarmTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,9 +22,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             SelfUnlockAlarmTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    AlarmScreen(modifier = Modifier.padding(innerPadding))
+                    AlarmScreen(
+                        modifier = Modifier.padding(innerPadding),
+                    )
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AlarmScreenPreview() {
+    SelfUnlockAlarmTheme {
+        AlarmScreen()
     }
 }
